20fcea8c6ae6e6ea44ca75d848a86622f2307410
im-o <frizgt@gmail.com>
2020-02-01 15:50:00 +0800

    Done testing show data on recyclerview

--- app/src/main/java/com/stimednp/crudfirebasefirestore/activity/MainActivity.kt
+++ app/src/main/java/com/stimednp/crudfirebasefirestore/activity/MainActivity.kt
@@ -13,14 +13,14 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.stimednp.crudfirebasefirestore.R
-import com.stimednp.crudfirebasefirestore.bind.UsersViewHolder
+import com.stimednp.crudfirebasefirestore.bind.AllUserAdapter
 import com.stimednp.crudfirebasefirestore.model.Users
 import com.stimednp.crudfirebasefirestore.utils.Const.PATH_AGE
 import com.stimednp.crudfirebasefirestore.utils.Const.PATH_COLLECTION
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mAdapter: FirestoreRecyclerAdapter<Users, UsersViewHolder>
+    private lateinit var mAdapter: FirestoreRecyclerAdapter<Users, AllUserAdapter.UsersViewHolder>
     private val mFirestore = FirebaseFirestore.getInstance()
     private val mUsersCollection = mFirestore.collection(PATH_COLLECTION)
     private val mQuery = mUsersCollection.orderBy(PATH_AGE, Query.Direction.ASCENDING)
@@ -53,58 +53,59 @@ class MainActivity : AppCompatActivity() {
             .setQuery(mQuery, Users::class.java)
             .build()
 
-        mAdapter = object : FirestoreRecyclerAdapter<Users, UsersViewHolder>(options) {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
-                return UsersViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_list_user, parent, false))
-            }
-
-            override fun onBindViewHolder(viewHolder: UsersViewHolder, position: Int, model: Users) {
-                viewHolder.bindItem(model)
-                viewHolder.itemView.setOnClickListener {
-                    showDialogMenu(model)
-                }
-            }
-        }
+//        mAdapter = object : FirestoreRecyclerAdapter<Users, UsersViewHolder>(options) {
+//            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
+//                return UsersViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_list_user, parent, false))
+//            }
+//
+//            override fun onBindViewHolder(viewHolder: UsersViewHolder, position: Int, model: Users) {
+//                viewHolder.bindItem(model)
+//                viewHolder.itemView.setOnClickListener {
+//                    showDialogMenu(model)
+//                }
+//            }
+//        }
+        mAdapter = AllUserAdapter(this, mUsersCollection, options)
         mAdapter.notifyDataSetChanged()
         rv_firedb.adapter = mAdapter
     }
 
-    private fun showDialogMenu(users: Users) {
-        //dialog popup edit hapus
-        val builder = AlertDialog.Builder(this@MainActivity, R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
-        val option = arrayOf("Edit", "Hapus")
-        builder.setItems(option) { dialog, which ->
-            when (which) {
-                //0 -> untuk berpindah ke activity AddEditActivity untuk edit dengan membawa data
-                0 -> startActivity(Intent(this, AddEditActivity::class.java).apply {
-                    putExtra(AddEditActivity.REQ_EDIT, true)
-                    putExtra(AddEditActivity.EXTRA_DATA, users)
-                })
-                1 -> showDialogDel(users.strId)
-            }
-        }
-        builder.create().show()
-    }
+//    private fun showDialogMenu(users: Users) {
+//        //dialog popup edit hapus
+//        val builder = AlertDialog.Builder(this@MainActivity, R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
+//        val option = arrayOf("Edit", "Hapus")
+//        builder.setItems(option) { dialog, which ->
+//            when (which) {
+//                //0 -> untuk berpindah ke activity AddEditActivity untuk edit dengan membawa data
+//                0 -> startActivity(Intent(this, AddEditActivity::class.java).apply {
+//                    putExtra(AddEditActivity.REQ_EDIT, true)
+//                    putExtra(AddEditActivity.EXTRA_DATA, users)
+//                })
+//                1 -> showDialogDel(users.strId)
+//            }
+//        }
+//        builder.create().show()
+//    }
 
-    private fun showDialogDel(strId: String) {
-        //dialog pop delete
-        val builder = AlertDialog.Builder(this, R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
-            .setTitle("Hapus Data")
-            .setMessage("Yakin mau hapus?")
-            .setPositiveButton(android.R.string.yes) { dialog, which ->
-                deleteById(strId)
-            }
-            .setNegativeButton(android.R.string.cancel, null)
-        builder.create().show()
-    }
+//    private fun showDialogDel(strId: String) {
+//        //dialog pop delete
+//        val builder = AlertDialog.Builder(this, R.style.ThemeOverlay_MaterialComponents_Dialog_Alert)
+//            .setTitle("Hapus Data")
+//            .setMessage("Yakin mau hapus?")
+//            .setPositiveButton(android.R.string.yes) { dialog, which ->
+//                deleteById(strId)
+//            }
+//            .setNegativeButton(android.R.string.cancel, null)
+//        builder.create().show()
+//    }
 
-    private fun deleteById(id: String) {
-        //menghapus data berdasarkan id
-        mUsersCollection.document(id)
-            .delete()
-            .addOnCompleteListener { Toast.makeText(this, "Succes Hapus data", Toast.LENGTH_SHORT).show() }
-            .addOnFailureListener { Toast.makeText(this, "Gagal Hapus data", Toast.LENGTH_SHORT).show() }
-    }
+//    private fun deleteById(id: String) {
+//        //menghapus data berdasarkan id
+//        mUsersCollection.document(id)
+//            .delete()
+//            .addOnCompleteListener { Toast.makeText(this, "Succes Hapus data", Toast.LENGTH_SHORT).show() }
+//            .addOnFailureListener { Toast.makeText(this, "Gagal Hapus data", Toast.LENGTH_SHORT).show() }
+//    }
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/stimednp/crudfirebasefirestore/bind/UsersViewHolder.kt
+++ app/src/main/java/com/stimednp/crudfirebasefirestore/bind/UsersViewHolder.kt
@@ -9,16 +9,16 @@ import kotlinx.android.synthetic.main.item_list_user.view.*
  * Created by rivaldy on 12/25/2019.
  */
 
-class UsersViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    fun bindItem(users: Users) {
-        view.apply {
-            val name = "Nama   : ${users.strName}"
-            val addr = "Alamat : ${users.strAddress}"
-            val age = "Umur    : ${users.intAge.toString()}"
-
-            tv_name.text = name
-            tv_adress.text = addr
-            tv_age.text = age
-        }
-    }
-}
+//class UsersViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+//    fun bindItem(users: Users) {
+//        view.apply {
+//            val name = "Nama   : ${users.strName}"
+//            val addr = "Alamat : ${users.strAddress}"
+//            val age = "Umur    : ${users.intAge.toString()}"
+//
+//            tv_name.text = name
+//            tv_adress.text = addr
+//            tv_age.text = age
+//        }
+//    }
+//}
